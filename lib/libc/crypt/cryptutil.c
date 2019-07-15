98091f59ca392dc0501794e0f1952fbae0ba044d
niklas <niklas@appli.se>
2019-07-15 10:31:34 +0200

    New version of support for deprecated password encryption, this time so both MD5 and DES passwords can be checked.

--- lib/libc/crypt/cryptutil.c
+++ lib/libc/crypt/cryptutil.c
@@ -21,6 +21,9 @@
 #include <login_cap.h>
 #include <errno.h>
 
+int _descrypt_checkpass(const char *, const char *);
+int _md5crypt_checkpass(const char *, const char *);
+
 int
 crypt_checkpass(const char *pass, const char *goodhash)
 {
@@ -36,12 +39,19 @@ crypt_checkpass(const char *pass, const char *goodhash)
 	if (strlen(goodhash) == 0 && strlen(pass) == 0)
 		return 0;
 
-	if (goodhash[0] == '$' && goodhash[1] == '2') {
-		if (bcrypt_checkpass(pass, goodhash))
-			goto fail;
-		return 0;
-	} else if (goodhash[0] == '$' && goodhash[1] == '1') {
-		if (memcmp(goodhash, md5crypt(pass, goodhash), strlen(goodhash)))
+	if (goodhash[0] == '$')
+		switch (goodhash[1]) {
+		case '1':
+			if (_md5crypt_checkpass(pass, goodhash))
+				goto fail;
+			return 0;
+		case '2':
+			if (bcrypt_checkpass(pass, goodhash))
+				goto fail;
+			return 0;
+		}
+	else {
+		if (_descrypt_checkpass(pass, goodhash))
 			goto fail;
 		return 0;
 	}
