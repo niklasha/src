eda2d0c34b0f47d8163a641363dec1398f020dfb
Jonathan Gray <jsg@jsg.id.au>
2021-09-14 11:12:55 +1000

    make drm_auth.c build

--- sys/dev/pci/newdrm/include/linux/lockdep.h
+++ sys/dev/pci/newdrm/include/linux/lockdep.h
@@ -13,6 +13,7 @@ struct pin_cookie {
 #define might_lock_nested(lock, subc)
 #define lockdep_assert_held(lock)	do { (void)(lock); } while(0)
 #define lockdep_assert_held_once(lock)	do { (void)(lock); } while(0)
+#define lockdep_assert_once(lock)	do { (void)(lock); } while(0)
 #define lock_acquire(lock, a, b, c, d, e, f)
 #define lock_release(lock, a)
 #define lock_acquire_shared_recursive(lock, a, b, c, d)
@@ -21,6 +22,7 @@ struct pin_cookie {
 #define lockdep_set_class(a, b)
 #define lockdep_init_map(a, b, c, d)
 #define lockdep_set_class_and_name(a, b, c)
+#define lockdep_is_held(lock)		0
 
 #define mutex_acquire(a, b, c, d)
 #define mutex_release(a, b)
