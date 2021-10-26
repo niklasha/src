23dd00a461c73422dba23bb39bbe9a6c4b83946e
niklas <niklas@appli.se>
2021-10-26 21:25:58 +0200

    Add ddb trigging type/size variables, to facilitate debugging of certain kernel malloc allocations.

--- sys/kern/kern_malloc.c
+++ sys/kern/kern_malloc.c
@@ -140,6 +140,11 @@ struct timeval malloc_errintvl = { 5, 0 };
 struct timeval malloc_lasterr;
 #endif
 
+#ifdef DDB
+int malloc_ddb_type = -1;
+int malloc_ddb_size = 0;
+#endif
+
 /*
  * Allocate a block of memory
  */
@@ -352,6 +357,10 @@ out:
 
 	TRACEPOINT(uvm, malloc, type, va, size, flags);
 
+#ifdef DDB
+	if (malloc_ddb_type >= 0 && malloc_ddb_size && type == malloc_ddb_type && size == malloc_ddb_size)
+		db_enter();
+#endif
 	return (va);
 }
 
