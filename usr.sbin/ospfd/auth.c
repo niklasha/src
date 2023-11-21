3f62d94205d30a9cc5e3b500104b2628a297ae65
bluhm <bluhm@openbsd.org>
2023-11-20 18:04:29 -0600

    iface->auth_key is not a real C string so use strnlen() to define the maximum.
    This fixes the use of 8 char passwords with auth simple.
    Reported by Laurent CARON (lcaron at unix-scripts info)
    from claudio@; OK tb@
    
    this is errata/7.4/004_ospfd.patch.sig

--- usr.sbin/ospfd/auth.c
+++ usr.sbin/ospfd/auth.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: auth.c,v 1.22 2023/07/03 09:40:47 claudio Exp $ */
+/*	$OpenBSD: auth.c,v 1.22.2.1 2023/11/20 18:04:29 bluhm Exp $ */
 
 /*
  * Copyright (c) 2004, 2005 Esben Norby <norby@openbsd.org>
@@ -166,7 +166,8 @@ auth_gen(struct ibuf *buf, struct iface *iface)
 			fatalx("auth_gen: ibuf_set failed");
 
 		if (ibuf_set(buf, offsetof(struct ospf_hdr, auth_key),
-		    iface->auth_key, strlen(iface->auth_key)) == -1)
+		    iface->auth_key, strnlen(iface->auth_key,
+		    sizeof(iface->auth_key))) == -1)
 			fatalx("auth_gen: ibuf_set failed");
 		break;
 	case AUTH_CRYPT:
