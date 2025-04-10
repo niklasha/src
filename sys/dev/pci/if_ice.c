b000ed4cde60920cdf589f71a21223e222a876d0
stsp <stsp@openbsd.org>
2025-04-10 07:06:44 +0000

    always call ifq_restart() if ice_txeof() makes space on a full Tx ring
    bluhm's udpbench tests have exposed a race where ice_txeof() makes space
    on a full (OACTIVE) Tx ring, yet for some reason the OACTIVE flag is not
    set while the interrupt handler is checking it.
    The interrupt handler would then fail to call ifq_restart(), and the
    driver would get stuck in OACTIVE state with no more traffic passing
    until ifconfig down/up.
    
    This patch is a workaround which makes all of bluhm's stress tests pass.
    I will keep trying to find a better fix, but this workaround is good
    enough for release.

--- sys/dev/pci/if_ice.c
+++ sys/dev/pci/if_ice.c
@@ -29457,7 +29457,7 @@ ice_txeof(struct ice_softc *sc, struct ice_tx_queue *txq)
 	struct ice_tx_desc *ring, *txd;
 	struct ice_tx_map *txm;
 	bus_dmamap_t map;
-	unsigned int cons, prod, last;
+	unsigned int cons, prod, last, free;
 	unsigned int mask;
 	uint64_t dtype;
 	int done = 0;
@@ -29467,6 +29467,11 @@ ice_txeof(struct ice_softc *sc, struct ice_tx_queue *txq)
 
 	if (cons == prod)
 		return (0);
+
+	free = cons;
+	if (free <= prod)
+		free += txq->desc_count;
+	free -= prod;
 
 	bus_dmamap_sync(sc->sc_dmat, ICE_DMA_MAP(&txq->tx_desc_mem),
 	    0, ICE_DMA_LEN(&txq->tx_desc_mem), BUS_DMASYNC_POSTREAD);
@@ -29512,6 +29517,15 @@ ice_txeof(struct ice_softc *sc, struct ice_tx_queue *txq)
 
 	if (done && ifq_is_oactive(ifq))
 		ifq_restart(ifq);
+	else if (free <= ICE_MIN_DESC_COUNT + 1) {
+		/* XXX sometimes ring was OACTIVE but is_oactive() is false */
+		free = cons;
+		if (free <= prod)
+			free += txq->desc_count;
+		free -= prod;
+		if (free > ICE_MIN_DESC_COUNT + 1)
+			ifq_restart(ifq);
+	}
 
 	return (done);
 }
