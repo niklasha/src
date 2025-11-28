a80a223b66547db36d1c0c1e4ea4eaa84d4aefa6
bluhm <bluhm@openbsd.org>
2025-11-28 11:27:27 -0700

    Ignore any iterator when traversing nd6 list.
    nd6_rtrequest() could crash with a NULL pointer dereference if an
    interator in nd6_list was inspected.  Skip freeing neigbor discovery
    entries and optimization in this unlikely case and try again later.
    
    reported by Mischa and Anton Kasimov; OK mvs@
    
    this is errata/7.8/011_nd6.patch.sig

--- sys/netinet6/nd6.c
+++ sys/netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nd6.c,v 1.303 2025/09/16 09:19:43 florian Exp $	*/
+/*	$OpenBSD: nd6.c,v 1.303.2.1 2025/11/28 18:27:27 bluhm Exp $	*/
 /*	$KAME: nd6.c,v 1.280 2002/06/08 19:52:07 itojun Exp $	*/
 
 /*
@@ -828,6 +828,9 @@ nd6_rtrequest(struct ifnet *ifp, int req, struct rtentry *rt)
 
 				ln_end = TAILQ_LAST(&nd6_list, llinfo_nd6_head);
 				if (ln_end == ln)
+					break;
+				/* cannot move the iterator, try next time */
+				if (ln_end->ln_rt == NULL)
 					break;
 
 				/* Move this entry to the head */
