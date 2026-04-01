0e4339eaab95a658e8e38b73d773b51a6a671028
bluhm <bluhm@openbsd.org>
2026-04-01 14:02:24 -0600

    Add SA state check for CREATE_CHILD_SA exchange, similar to what we do for INFORMATIONAL exchanges. iked currently assumes that IKE_AUTH always results in valid child SAs, so IKEV2_STATE_ESTABLISHED means we have successfully completed the IKE_AUTH exchange for the SA.
    Independently found by Dirk Loss and Daniel Polak (SYS.nl)
    from tobhe@; ok and discussed with markus@ stsp@
    
    Add ikev2_validate_ef() to validate fragment payload header size field
    as we do for other IKEv2 payloads.
    
    Reported by Dirk Loss
    from tobhe@; ok markus@
    
    iked only ever handles one exchange at a time so we can drop the
    entire fragment queue instead of doing a lookup based on the msgid
    
    Found by Dirk Loss
    from tobhe@; ok markus@ hshoexer@
    
    If we receive a response it must have the exact same msgid as the last
    request we sent, so make sure they match exactly rather than allowing
    higher msgids.
    We can't handle out of order responses and if we don't receive a response
    in time we retransmit our request until we get one or run into a timeout.
    
    Found by Dirk Loss
    from tobhe@; ok markus@ hshoexer@
    
    this is errata/7.8/027_iked.patch.sig

--- sbin/iked/ikev2.c
+++ sbin/iked/ikev2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ikev2.c,v 1.394 2025/07/31 19:02:43 pascal Exp $	*/
+/*	$OpenBSD: ikev2.c,v 1.394.2.1 2026/04/01 20:02:24 bluhm Exp $	*/
 
 /*
  * Copyright (c) 2019 Tobias Heider <tobias.heider@stusta.de>
@@ -713,7 +713,7 @@ ikev2_recv(struct iked *env, struct iked_message *msg)
 	}
 
 	if (msg->msg_response) {
-		if (msg->msg_msgid > sa->sa_reqid) {
+		if (msg->msg_msgid + 1 != sa->sa_reqid) {
 			ikestat_inc(env, ikes_msg_rcvd_dropped);
 			return;
 		}
@@ -4452,6 +4452,9 @@ ikev2_init_create_child_sa(struct iked *env, struct iked_message *msg)
 	uint32_t			 spi32;
 	int				 pfs = 0, ret = -1;
 
+	if (!sa_stateok(sa, IKEV2_STATE_ESTABLISHED))
+		return -1;
+
 	if (!ikev2_msg_frompeer(msg) ||
 	    (sa->sa_stateflags & (IKED_REQ_CHILDSA|IKED_REQ_INF)) == 0)
 		return (0);
@@ -4916,6 +4919,9 @@ ikev2_resp_create_child_sa(struct iked *env, struct iked_message *msg)
 	int				 initiator, protoid, rekeying = 1;
 	int				 ret = -1;
 	int				 pfs = 0;
+
+	if (!sa_stateok(sa, IKEV2_STATE_ESTABLISHED))
+		return -1;
 
 	initiator = sa->sa_hdr.sh_initiator ? 1 : 0;
 
